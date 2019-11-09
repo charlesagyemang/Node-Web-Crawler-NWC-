df4fe36f9eaea35a9e4b768826cf5083c3c5ec6d
charlesagyemang <micnkru@gmail.com>
2019-11-09 12:48:54 +0000

    --

--- src/modules/crawl/test/crawl.controller.test.js
+++ src/modules/crawl/test/crawl.controller.test.js
@@ -1,5 +1,5 @@
-describe('User::Controller', async () => {
-  it('user controller test', async () => {
+describe('Crawl::Controller', async () => {
+  it('crawl controller test', async () => {
     expect(1 + 1).toBe(2);
   });
 });
