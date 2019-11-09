c670234938f5849c3dd41416790114162c32a848
charlesagyemang <micnkru@gmail.com>
2019-11-09 12:45:40 +0000

    Test: Write Test For Ping Route

--- src/modules/crawl/test/crawl.routes.test.js
+++ src/modules/crawl/test/crawl.routes.test.js
@@ -1,13 +1,13 @@
-// import HTTPStatus from 'http-status';
-// import request from 'supertest-as-promised';
-// import server from '../../../server';
+import HTTPStatus from 'http-status';
+import request from 'supertest-as-promised';
+import server from '../../../server';
 
 describe('Crowl::Routes', async () => {
-  it.skip('should crawl given website successfully', async () => {
+  it('Should Ping Crawl Routes Successfully', async () => {
+    const res = await request(server).get('/api/crawl/ping');
 
-    //
-    // expect(res.statusCode).toBe(HTTPStatus.OK);
-    // expect(res.body).toHaveProperty('_id');
-    // expect(res.body).toHaveProperty('token');
+    expect(res.statusCode).toBe(HTTPStatus.OK);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toBe('Heyy Me That I Am Model Crawl');
   });
 });
